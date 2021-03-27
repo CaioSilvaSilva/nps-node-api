fee123111f3554881c020604f34abae7da912877
Caio Poli <caio.poli@ernetwork.com.br>
2021-03-27 18:23:12 -0300

    :card_file_box: Create migration users

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { request } from 'express';
+import "./database";
 
 const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({ message: "NLW04" });
+});
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Os dados foram salvos com sucesso!" });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
